dbf0fb78a3e69545517ed176f63acd8c9b20936e
Gavin Asay <gavin.asay@gmail.com>
2021-07-24 04:05:05 -0600

    init

---
+++ src/main/java/com/inscheduler/InschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.inscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InschedulerApplication.class, args);
+	}
+
+}
